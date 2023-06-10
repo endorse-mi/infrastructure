1f22103b13015ac3f17912fef2d57a1f8b6ef674
Stephen Fong <fongchinghinunsw@gmail.com>
2023-06-10 14:53:48 +1000

    Revert "Change localhost port"
    This reverts commit 1f352265ed58ee1b8970a361752d9131ad52b8ad.

--- lib/user/cognito.ts
+++ lib/user/cognito.ts
@@ -6,8 +6,12 @@ import { ENVIRONMENT } from '../config';
 
 const OAuthCallbackUrls =
   ENVIRONMENT === 'prod'
-    ? ['https://endorse-mi.koala-techs.com/api/auth/callback/cognito', 'https://prod.endorse-mi.koala-techs.com/api/auth/callback/cognito']
-    : ['http://localhost:8080/api/auth/callback/cognito', 'https://dev.endorse-mi.koala-techs.com/api/auth/callback/cognito'];
+    ? [
+        'http://localhost:3000/api/auth/callback/cognito',
+        'https://endorse-mi.koala-techs.com/api/auth/callback/cognito',
+        'https://prod.endorse-mi.koala-techs.com/api/auth/callback/cognito',
+      ]
+    : ['http://localhost:3000/api/auth/callback/cognito', 'https://dev.endorse-mi.koala-techs.com/api/auth/callback/cognito'];
 
 export class Cognito {
   constructor(scope: Construct) {
